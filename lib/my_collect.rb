0b3fc6fd0be3a1f887e5ff7ef5b323e787e731ca
Arslan Ali <ali.arslan319@gmail.com>
2019-03-28 01:31:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array, collection)
   new_collection = []
     collection.each do |collected|
       collected.upcase
+    end
     array.each do |names|
       names.split(" ").first
     end
-
 end
