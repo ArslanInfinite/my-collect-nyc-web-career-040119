ae648d50a3aec8f2b899431915de46066ad934da
Arslan Ali <ali.arslan319@gmail.com>
2019-03-28 02:20:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(collection)
   new_collection = []
   counter = 0
   loop do
-      new_collection << yield(collection[0])
+      new_collection << yield(collection[0, 1, 2, 3])
       if counter >= collection.length - 1
       break
       end
